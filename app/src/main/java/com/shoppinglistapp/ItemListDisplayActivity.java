1407e087e6f6c37963f4517032147664b2456856
nrajeevlochan <nrajeevlochan@gmail.com>
2015-10-21 19:24:57 -0500

    Added below items 1. added ItemtouchHelper class to handle swipe to delete feature 2. added dialog_input.xml and ItemInputDialog.java files, missed to add these file in previous commit.

--- app/src/main/java/com/shoppinglistapp/ItemListDisplayActivity.java
+++ app/src/main/java/com/shoppinglistapp/ItemListDisplayActivity.java
@@ -42,8 +42,7 @@ public class ItemListDisplayActivity extends AppCompatActivity implements ItemIn
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                mItemDialog.show(getFragmentManager(), DIALOG_TAG);
             }
         });
 
