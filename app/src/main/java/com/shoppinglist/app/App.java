970987c748300321f781a37c7f2d90c7c5c2fde3
nrajeevlochan <nrajeevlochan@gmail.com>
2016-05-03 01:40:20 -0500

    To add Application file to project

---
+++ app/src/main/java/com/shoppinglist/app/App.java
@@ -0,0 +1,27 @@
+package com.shoppinglist.app;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.shoppinglist.db.DataBaseHelper;
+import com.shoppinglist.db.DataBaseAdapter;
+
+/**
+ * Created by ${} on 5/2/16.
+ */
+public class App extends Application {
+    private static Context context;
+    private static DataBaseHelper dbHelper;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = this.getApplicationContext();
+        dbHelper = new DataBaseHelper();
+        DataBaseAdapter.initializeInstance(dbHelper);
+    }
+
+    public static Context getContext() {
+        return context;
+    }
+}
